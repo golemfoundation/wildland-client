351b27b0cd7050facb4a1e6a2344d556a6ed4e98
Paweł Marczewski <pawel@invisiblethingslab.com>
2020-04-09 12:36:28 +0200

    Update Docker setup
    - Run as normal user, not root (but make sudo available)
    - Use mounted scripts instead of copying them (to avoid rebuilding)

--- docker/init.sh
+++ docker/init.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+MOUNT_DIR="$HOME/mnt"
+mkdir "$MOUNT_DIR"
+
 export __fish_prompt_hostname="wildland-fuse"
 cd /wildland-fuse
-/sbin/mount.fuse ./wildland-fuse /mnt \
+./wildland-fuse "$MOUNT_DIR" \
   -o manifest=./example/manifests/container1.yaml,manifest=./example/manifests/container2.yaml
-cd /mnt
-fish
+cd "$MOUNT_DIR"
+exec fish
