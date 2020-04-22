f604fc2aad8c87b7255609096966342895ceba48
Paweł Marczewski <pawel@invisiblethingslab.com>
2020-04-22 13:29:28 +0200

    docker: add editor, add 'wl' to path

--- docker/init.sh
+++ docker/init.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 MOUNT_DIR="$HOME/mnt"
+export EDITOR=nano
+export PATH=/wildland-fuse:$PATH
+
 mkdir "$MOUNT_DIR"
 
 export __fish_prompt_hostname="wildland-fuse"
-cd /wildland-fuse
 
 mkdir -p ~/.wildland
 echo "mount_dir: $MOUNT_DIR" > ~/.wildland/config.yaml
 
-./example/setup.sh
+/wildland-fuse/example/setup.sh
 
 cd "$MOUNT_DIR"
 exec fish
