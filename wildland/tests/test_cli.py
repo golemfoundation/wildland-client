1d11932cbf9547eb377772c9a823420235b7531c
Paweł Marczewski <pawel@invisiblethingslab.com>
2020-04-30 13:49:25 +0200

    Add a test for mount paths

--- wildland/tests/test_cli.py
+++ wildland/tests/test_cli.py
@@ -21,6 +21,7 @@
 
 import shutil
 import json
+import os
 
 import yaml
 import pytest
@@ -210,13 +211,34 @@ def test_container_mount(cli, base_dir):
     cli('storage', 'create', 'Storage', '--type', 'local', '--path', '/PATH',
         '--container', 'Container', '--update-container')
 
+    with open(base_dir / 'containers/Container.yaml') as f:
+        documents = list(yaml.safe_load_all(f))
+    path = documents[1]['paths'][0]
+
     cli('container', 'mount', 'Container')
 
     # The command should write container manifest to .control/mount.
     with open(base_dir / 'mnt/.control/mount') as f:
-        data = f.read()
-    assert '"signer": "0xaaa"' in data
-    assert "/PATH" in data
+        command = json.load(f)
+    assert command['storage']['signer'] == '0xaaa'
+    assert command['paths'] == [
+        f'/.users/0xaaa{path}',
+        f'/.users/0xaaa/PATH',
+        path,
+        f'/PATH',
+    ]
+
+    modify_file(base_dir / 'config.yaml', "default_user: '0xaaa'", '')
+
+    # The command should not contain the default path.
+    cli('container', 'mount', 'Container')
+    with open(base_dir / 'mnt/.control/mount') as f:
+        command = json.load(f)
+    assert command['storage']['signer'] == '0xaaa'
+    assert command['paths'] == [
+        f'/.users/0xaaa{path}',
+        f'/.users/0xaaa/PATH',
+    ]
 
 
 def test_container_unmount(cli, base_dir):
