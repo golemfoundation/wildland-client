95a54a431b62a86574c45166b0bd4d2913cde0f6
Paweł Marczewski <pawel@invisiblethingslab.com>
2020-06-05 12:03:53 +0200

    Fix typo in package name

--- plugins/s3/setup.py
+++ plugins/s3/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="widland-s3",
+    name="wildland-s3",
     version="0.1",
     packages=find_packages(),
     entry_points={
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="widland-fuse",
+    name="wildland-fuse",
     version="0.1",
     packages=find_packages(),
     entry_points={
