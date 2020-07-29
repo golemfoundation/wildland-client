20c5998d81f99320a7f97d8db691fb2084dee78d
Paweł Marczewski <pawel@invisiblethingslab.com>
2020-07-29 14:18:52 +0200

    Lint fixes

--- wildland/storage_backends/date_proxy.py
+++ wildland/storage_backends/date_proxy.py
@@ -17,6 +17,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+'''
+Date proxy backend
+'''
 
 from typing import Tuple, Optional, Iterable
 from pathlib import PurePosixPath
--- wildland/tests/test_date_proxy.py
+++ wildland/tests/test_date_proxy.py
@@ -23,7 +23,6 @@
 from pathlib import Path
 import os
 from datetime import datetime
-import glob
 
 import pytest
 
