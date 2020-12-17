7f04300ca58497caf14aa8b09655e8f0052a6322
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2020-12-17 06:54:27 +0100

    StorageBackend: split backend_id calculation into separate function
    This will be useful in a moment.

--- wildland/storage_backends/base.py
+++ wildland/storage_backends/base.py
@@ -186,9 +186,22 @@ class StorageBackend(metaclass=abc.ABCMeta):
         self.hash_db = None
         self.mounted = 0
 
+        self.backend_id = self.get_backend_id(self.params)
+
+    @staticmethod
+    def get_backend_id(params: dict) -> str:
+        """
+        Get an backend_id - a unique identifier of a StorageBackend with given parameters
+
+        :param params: StorageBackend params
+        :return: str
+        """
         hasher = hashlib.sha256()
-        hasher.update(yaml.dump(self.params, sort_keys=True).encode('utf-8'))
-        self.backend_id = hasher.hexdigest()
+        # skip 'storage' object if present, it is derived from reference-container
+        params_for_hash = dict((k, v) for (k, v) in params.items()
+                               if k != 'storage')
+        hasher.update(yaml.dump(params_for_hash, sort_keys=True).encode('utf-8'))
+        return hasher.hexdigest()
 
     @classmethod
     def cli_options(cls) -> List[click.Option]:
