a000802a1c25944be6c39388cb25ce9899ab3d09
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2021-03-01 17:39:46 +0100

    Do not interrupt mounting containers when WL path is used
    Continue to mount other containers resolved from a single WL path, even
    if some errors occurred. This is especially useful when using wildcard
    WL path.
    
    Sadly, this does require nested the same try/except - one wrapping the
    whole 'containers' iteration (to catch errors on listing containers) and
    the other one on mounting individual container (to not interrupt the
    whole iteration when one container fails to mount).

--- wildland/cli/cli_container.py
+++ wildland/cli/cli_container.py
@@ -587,11 +587,21 @@ def mount(obj: ContextObj, container_names, remount, save, import_users: bool,
                 )
             else:
                 containers = obj.client.load_containers_from(container_name)
+        except WildlandError as ex:
+            failed = True
+            exc_msg += str(ex) + '\n'
+            continue
+
+        try:
             for container in containers:
-                user_paths = obj.client.get_bridge_paths_for_user(container.owner)
-                params.extend(prepare_mount(
-                    obj, container, str(container.local_path), user_paths,
-                    remount, with_subcontainers, None, quiet, only_subcontainers))
+                try:
+                    user_paths = obj.client.get_bridge_paths_for_user(container.owner)
+                    params.extend(prepare_mount(
+                        obj, container, str(container), user_paths,
+                        remount, with_subcontainers, None, quiet, only_subcontainers))
+                except WildlandError as ex:
+                    failed = True
+                    exc_msg += str(ex) + '\n'
         except WildlandError as ex:
             failed = True
             exc_msg += str(ex) + '\n'
