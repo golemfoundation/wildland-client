golemfoundation/wildland-client

commit c31cbc4a25c85b683d9bb7f10d81007e301d9ff2

cli: add global exception handler

wildland/cli/cli_base.py
@@ -22,6 +22,8 @@ Wildland command-line interface - base module.
 """
 
 import collections
+import sys
+import traceback
 from pathlib import Path
 from typing import List, Tuple, Callable
 
@@ -35,7 +37,6 @@ class CliError(WildlandError, click.ClickException):
     """
     User error during CLI command execution
     """
-
 
 # pylint: disable=no-self-use
 
@@ -56,6 +57,21 @@ class AliasedGroup(click.Group):
     def __init__(self, *args, **kwds):
         super().__init__(*args, **kwds)
         self.aliases = {}
+        self.debug = False
+
+    def __call__(self, *args, **kwargs):
+        try:
+            return self.main(*args, **kwargs)
+        except Exception as exc:
+            click.echo(f'Error: {exc}')
+            if self.debug is True:
+                traceback.print_exception(*sys.exc_info())
+
+            if isinstance(exc, click.ClickException):
+                # pylint: disable=no-member
+                sys.exit(exc.exit_code)
+            else:
+                sys.exit(1)
 
     def command(self, *args, **kwargs):
         if 'alias' not in kwargs:
@@ -76,10 +92,14 @@ class AliasedGroup(click.Group):
 
         >>> cmd.add_alias(alias='original-command')
         """
-        assert all(alias not in (*self.aliases, *self.commands) for alias in kwds)
+        assert all(
+            alias not in (*self.aliases, *self.commands) for alias in kwds)
         self.aliases.update(kwds)
 
     def get_command(self, ctx, cmd_name):
+        if self.name == 'wl':
+            self.debug = ctx.params['debug']
+
         # 1) try exact command
         rv = super().get_command(ctx, cmd_name)
         if rv is not None:

wildland/tests/test_cli.py
@@ -33,11 +33,11 @@ from unittest.mock import patch
 import pytest
 import yaml
 
-from click import ClickException
-from ..manifest.manifest import ManifestError, Manifest
 from ..cli.cli_base import CliError
-from ..utils import load_yaml, load_yaml_all
 from ..cli.cli_common import del_nested_field
+from ..exc import WildlandError
+from ..manifest.manifest import ManifestError, Manifest
+from ..utils import load_yaml, load_yaml_all
 
 
 def modify_file(path, pattern, replacement):
@@ -1710,13 +1710,13 @@ def test_container_mount_check_trusted_owner(cli, base_dir, control_client):
     # Should not mount if the storage is not trusted
 
     control_client.expect('info', make_info(None))
-    with pytest.raises(ClickException, match='Signature expected'):
+    with pytest.raises(WildlandError, match='Signature expected'):
         cli('container', 'mount', manifest_path)
 
     # Should not mount if the owner is different
 
     control_client.expect('info', make_info('0xbbb'))
-    with pytest.raises(ClickException, match='Wrong owner for manifest without signature'):
+    with pytest.raises(WildlandError, match='Wrong owner for manifest without signature'):
         cli('container', 'mount', manifest_path)
 
     # Should mount if the storage is trusted and with right owner
@@ -1821,7 +1821,7 @@ backends:
     ]
 
 
-def test_container_mount_errors(cli, cli_fail, base_dir, control_client, tmp_path):
+def test_container_mount_errors(cli, base_dir, control_client, tmp_path):
     control_client.expect('status', {})
 
     cli('user', 'create', 'User', '--key', '0xaaa')
@@ -1863,9 +1863,10 @@ backends:
     control_client.expect('paths', {})
     control_client.expect('mount')
 
-    output = cli_fail('container', 'mount', tmp_path / 'container-*.yaml', capture=True)
-    assert 'Traceback' not in output
-    assert 'Failed to load some container manifests' in output
+    # TODO: cli_fail doesn't capture stderr now...
+    with pytest.raises(WildlandError, match='Failed to load some container manifests'):
+        output = cli('container', 'mount', tmp_path / 'container-*.yaml', capture=True)
+        assert 'Traceback' not in output
 
     # the other container should still be mounted
     command = control_client.calls['mount']['items']
@@ -1976,7 +1977,7 @@ def test_container_mount_container_without_storage(cli, control_client):
     cli('user', 'create', 'User', '--key', '0xaaa')
     cli('container', 'create', 'Container', '--path', '/PATH')
 
-    with pytest.raises(ClickException, match='No valid storages found'):
+    with pytest.raises(WildlandError, match='No valid storages found'):
         cli('container', 'mount', 'Container')
 
 
@@ -2501,13 +2502,13 @@ def test_cli_set_modify(cli, base_dir):
                                  [{'file': 't1.template.jinja', 'type': 'file'},
                                   {'file': 't3.template.jinja', 'type': 'inline'}]}
 
-    with pytest.raises(CliError):
+    with pytest.raises(WildlandError):
         cli('storage-set', 'modify', 'add-template', '-t', 't2', 'set123')
-    with pytest.raises(CliError):
+    with pytest.raises(WildlandError):
         cli('storage-set', 'modify', 'del-template', '-t', 't2', 'set123')
-    with pytest.raises(CliError):
+    with pytest.raises(WildlandError):
         cli('storage-set', 'modify', 'add-template', '-t', 't2123', 'set1')
-    with pytest.raises(CliError):
+    with pytest.raises(WildlandError):
         cli('storage-set', 'modify', 'del-template', '-t', 't2', 'set123')
 
 
@@ -2614,7 +2615,7 @@ def test_cli_set_missing_param(cli, base_dir):
     cli('storage-set', 'add', '--inline', 'title', 'set1')
     cli('user', 'create', 'User')
 
-    with pytest.raises(CliError, match='\'title\' is undefined'):
+    with pytest.raises(WildlandError, match='\'title\' is undefined'):
         cli('container', 'create',
             'Container', '--path', '/PATH', '--storage-set', 'set1', capture=True)
 

wildland/cli/cli_container.py
@@ -69,7 +69,6 @@ class OptionRequires(click.Option):
     """
     Helper class to provide conditional required for click.Option
     """
-
     def __init__(self, *args, **kwargs):
         try:
             self.required_opt = kwargs.pop('requires')
@@ -139,16 +138,13 @@ def create(obj: ContextObj, owner, path, name, update_user, default_storage_set,
         try:
             storage_set = TemplateManager(obj.client.template_dir).get_storage_set(set_name)
         except FileNotFoundError as fnf:
-            raise CliError(f'Storage set {set_name} not found.') from fnf
+            raise WildlandError(f'Storage set {set_name} not found.') from fnf
 
     if access and not encrypt_manifest:
         raise CliError('--no-encrypt and --access are mutually exclusive.')
 
     if access:
-        try:
-            access = [{'user': obj.client.load_user_by_name(user).owner} for user in access]
-        except WildlandError as ex:
-            raise CliError(f'Cannot create container: {ex}') from ex
+        access = [{'user': obj.client.load_user_by_name(user).owner} for user in access]
     elif not encrypt_manifest:
         access = [{'user': '*'}]
 
@@ -170,15 +166,15 @@ def create(obj: ContextObj, owner, path, name, update_user, default_storage_set,
         except FileNotFoundError as fnf:
             click.echo(f'Removing container: {path}')
             path.unlink()
-            raise CliError('Failed to create storage from set: storage set not found') from fnf
+            raise WildlandError('Failed to create storage from set: storage set not found') from fnf
         except ValueError as e:
             click.echo(f'Removing container: {path}')
             path.unlink()
-            raise CliError(f'Failed to create storage from set: {e}') from e
+            raise WildlandError(f'Failed to create storage from set: {e}') from e
 
     if update_user:
         if not owner.local_path:
-            raise CliError('Cannot update user because the manifest path is unknown')
+            raise WildlandError('Cannot update user because the manifest path is unknown')
         click.echo('Attaching container to user')
 
         owner.containers.append(str(obj.client.local_url(path)))
@@ -198,18 +194,18 @@ def update(obj: ContextObj, storage, cont):
     obj.client.recognize_users()
     container = obj.client.load_container_from(cont)
     if container.local_path is None:
-        raise ClickException('Can only update a local manifest')
+        raise WildlandError('Can only update a local manifest')
 
     if not storage:
-        print('No change')
+        click.echo('No change')
         return
 
     for storage_name in storage:
         storage = obj.client.load_storage_from(storage_name)
         assert storage.local_path
-        print(f'Adding storage: {storage.local_path}')
+        click.echo(f'Adding storage: {storage.local_path}')
         if str(storage.local_path) in container.backends:
-            raise click.ClickException('Storage already attached to container')
+            raise WildlandError('Storage already attached to container')
         container.backends.append(obj.client.local_url(storage.local_path))
 
     obj.client.save_container(container)
@@ -226,11 +222,7 @@ def publish(obj: ContextObj, cont):
 
     obj.client.recognize_users()
     container = obj.client.load_container_from(cont)
-    try:
-        obj.client.publish_container(container)
-    except WildlandError as exc:
-        print(str(exc))
-        sys.exit(1)
+    obj.client.publish_container(container)
 
 
 def _container_info(client, container):
@@ -272,10 +264,7 @@ def info(obj: ContextObj, name):
     """
 
     obj.client.recognize_users()
-    try:
-        container = obj.client.load_container_from(name)
-    except ManifestError as ex:
-        raise CliError(f'Failed to load manifest: {ex}') from ex
+    container = obj.client.load_container_from(name)
 
     _container_info(obj.client, container)
 
@@ -316,7 +305,7 @@ def delete(obj: ContextObj, name, force, cascade):
         return
 
     if not container.local_path:
-        raise CliError('Can only delete a local manifest')
+        raise WildlandError('Can only delete a local manifest')
 
     # unmount if mounted
     try:
@@ -436,12 +425,9 @@ def add_access(ctx, input_file, access):
 
     processed_access = []
 
-    try:
-        for user in access:
-            user = ctx.obj.client.load_user_by_name(user)
-            processed_access.append({'user': user.owner})
-    except WildlandError as ex:
-        raise CliError(f'Cannot modify access: {ex}') from ex
+    for user in access:
+        user = ctx.obj.client.load_user_by_name(user)
+        processed_access.append({'user': user.owner})
 
     modify_manifest(ctx, input_file, add_field, 'access', processed_access)
 
@@ -459,12 +445,9 @@ def del_access(ctx, input_file, access):
 
     processed_access = []
 
-    try:
-        for user in access:
-            user = ctx.obj.client.load_user_by_name(user)
-            processed_access.append({'user': user.owner})
-    except WildlandError as ex:
-        raise CliError(f'Cannot modify access: {ex}') from ex
+    for user in access:
+        user = ctx.obj.client.load_user_by_name(user)
+        processed_access.append({'user': user.owner})
 
     modify_manifest(ctx, input_file, del_field, 'access', processed_access)
 
@@ -552,14 +535,14 @@ def prepare_mount(obj: ContextObj,
     if not subcontainers or not only_subcontainers:
         if obj.fs_client.find_primary_storage_id(container) is None:
             if not quiet:
-                print(f'new: {container_name}')
+                click.echo(f'new: {container_name}')
             yield (container, storages, user_paths, subcontainer_of)
         elif remount:
             storages_to_remount = []
 
             for path in obj.fs_client.get_orphaned_container_storage_paths(container, storages):
                 storage_id = obj.fs_client.find_storage_id_by_path(path)
-                print(f'Removing orphaned storage {path} (id: {storage_id} )')
+                click.echo(f'Removing orphaned storage {path} (id: {storage_id} )')
                 obj.fs_client.unmount_storage(storage_id)
 
             for storage in storages:
@@ -567,14 +550,14 @@ def prepare_mount(obj: ContextObj,
                     storages_to_remount.append(storage)
 
                     if not quiet:
-                        print(f'changed: {storage.backend_id}')
+                        click.echo(f'changed: {storage.backend_id}')
                 else:
                     if not quiet:
-                        print(f'not changed: {storage.backend_id})')
+                        click.echo(f'not changed: {storage.backend_id})')
 
             yield (container, storages_to_remount, user_paths, subcontainer_of)
         else:
-            raise CliError(f'Already mounted: {container.local_path}')
+            raise WildlandError(f'Already mounted: {container.local_path}')
 
     if with_subcontainers:
         for subcontainer in subcontainers:
@@ -606,11 +589,8 @@ def mount(obj: ContextObj, container_names, remount, save, import_users: bool,
 
     The Wildland system has to be mounted first, see ``wl start``.
     """
-    try:
-        obj.fs_client.ensure_mounted()
-        obj.client.recognize_users()
-    except WildlandError as ex:
-        raise ClickException(ex) from ex
+    obj.fs_client.ensure_mounted()
+    obj.client.recognize_users()
 
     if import_users:
         obj.client.auto_import_users = True
@@ -678,7 +658,7 @@ def mount(obj: ContextObj, container_names, remount, save, import_users: bool,
                 {'default-containers': new_default_containers})
 
     if failed:
-        raise ClickException(exc_msg)
+        raise WildlandError(exc_msg)
 
 
 @container_.command(short_help='unmount container', alias=['umount'])
@@ -694,11 +674,8 @@ def unmount(obj: ContextObj, path: str, with_subcontainers: bool, container_name
     identify the container by one of its path (using ``--path``).
     """
 
-    try:
-        obj.fs_client.ensure_mounted()
-        obj.client.recognize_users()
-    except WildlandError as ex:
-        raise ClickException(ex) from ex
+    obj.fs_client.ensure_mounted()
+    obj.client.recognize_users()
 
     if bool(container_names) + bool(path) != 1:
         raise click.UsageError('Specify either container or --path')
@@ -730,7 +707,7 @@ def unmount(obj: ContextObj, path: str, with_subcontainers: bool, container_name
     else:
         storage_id = obj.fs_client.find_storage_id_by_path(PurePosixPath(path))
         if storage_id is None:
-            raise ClickException('Container not mounted')
+            raise WildlandError('Container not mounted')
         storage_ids = [storage_id]
         if with_subcontainers:
             storage_ids.extend(
@@ -738,14 +715,14 @@ def unmount(obj: ContextObj, path: str, with_subcontainers: bool, container_name
                     obj.fs_client.get_container_from_storage_id(storage_id)))
 
     if not storage_ids:
-        raise ClickException('No containers mounted')
+        raise WildlandError('No containers mounted')
 
     click.echo(f'Unmounting {len(storage_ids)} containers')
     for storage_id in storage_ids:
         obj.fs_client.unmount_storage(storage_id)
 
     if failed:
-        raise ClickException(exc_msg)
+        raise WildlandError(exc_msg)
 
 
 class Remounter:
@@ -882,7 +859,7 @@ def terminate_daemon(pfile, error_message):
             except ProcessLookupError:
                 os.remove(pfile)
     else:
-        raise ClickException(error_message)
+        raise WildlandError(error_message)
 
 
 @container_.command('mount-watch', short_help='mount container')
@@ -975,7 +952,7 @@ def sync_container(obj: ContextObj, target_remote, cont):
         target_storages = [[storage for storage in storages
                             if obj.client.is_local_storage(storage)][0]]
     except IndexError:
-        raise CliError('No local storage backend found')  # pylint: disable=raise-missing-from
+        raise WildlandError('No local storage backend found')  # pylint: disable=raise-missing-from
 
     default_remotes = obj.client.config.get('default-remote-for-container')
 
@@ -1020,7 +997,7 @@ def sync_container(obj: ContextObj, target_remote, cont):
         try:
             syncer.start_syncing()
         except FileNotFoundError as e:
-            print(f"Storage root not found! Details: {e}")
+            click.echo(f"Storage root not found! Details: {e}")
             return
         try:
             threading.Event().wait()
@@ -1061,8 +1038,8 @@ def list_container_conflicts(obj: ContextObj, cont, force_scan):
         hash_db = HashDb(obj.client.config.base_dir)
         conflicts = hash_db.get_conflicts(container.ensure_uuid())
         if conflicts is None:
-            print("No backends have been synced for this container; "
-                  "list-conflicts will not work without a preceding container sync")
+            click.echo("No backends have been synced for this container; "
+                       "list-conflicts will not work without a preceding container sync")
             return
     else:
         storages = [StorageBackend.from_params(storage.params) for storage in
@@ -1070,12 +1047,12 @@ def list_container_conflicts(obj: ContextObj, cont, force_scan):
         conflicts = list_storage_conflicts(storages)
 
     if conflicts:
-        print("Conflicts detected on:")
+        click.echo("Conflicts detected on:")
         for (path, c1, c2) in conflicts:
             # TODO: check if file still exists?
-            print(f"Conflict detected in file {path} in containers {c1} and {c2}")
+            click.echo(f"Conflict detected in file {path} in containers {c1} and {c2}")
     else:
-        print("No conflicts were detected by container sync.")
+        click.echo("No conflicts were detected by container sync.")
 
 
 @container_.command(short_help='duplicate a container')

wildland/cli/cli_main.py
@@ -52,17 +52,20 @@ from .. import __version__ as _version
 PROJECT_PATH = Path(__file__).resolve().parents[1]
 FUSE_ENTRY_POINT = PROJECT_PATH / 'wildland-fuse'
 
+
 @aliased_group('wl')
 @click.option('--dummy/--no-dummy', default=False,
-    help='use dummy signatures')
+              help='use dummy signatures')
 @click.option('--base-dir', default=None,
-    help='base directory for configuration')
+              help='base directory for configuration')
+@click.option('--debug/--no-debug', default=False,
+              help='print full traceback on exception')
 @click.option('--verbose', '-v', count=True,
               help='output logs (repeat for more verbosity)')
 @click.version_option(_version)
 @click.pass_context
-def main(ctx, base_dir, dummy, verbose):
-    # pylint: disable=missing-docstring
+def main(ctx, base_dir, dummy, debug, verbose):
+    # pylint: disable=missing-docstring, unused-argument
 
     client = Client(dummy=dummy, base_dir=base_dir)
     ctx.obj = ContextObj(client)
@@ -208,7 +211,6 @@ def status(obj: ContextObj, with_subcontainers):
             click.echo(f'  subcontainer-of: {storage["subcontainer_of"]}')
         click.echo()
 
-
 @main.command(short_help='renamed to "start"')
 def mount():
     """
@@ -249,4 +251,4 @@ def watch(obj: ContextObj, patterns, with_initial):
 
 
 if __name__ == '__main__':
-    main() # pylint: disable=no-value-for-parameter
+    main()  # pylint: disable=no-value-for-parameter

wildland/cli/cli_storage.py
@@ -32,7 +32,6 @@ from .cli_base import aliased_group, ContextObj, CliError
 from .cli_common import sign, verify, edit, modify_manifest, set_field, add_field, del_field, dump
 from ..storage import Storage
 from ..manifest.template import TemplateManager
-from ..manifest.schema import SchemaError
 
 from ..storage_backends.base import StorageBackend
 from ..storage_backends.dispatch import get_storage_backends
@@ -112,7 +111,7 @@ def _do_create(
 
     container = obj.client.load_container_from(container)
     if not container.local_path:
-        raise click.ClickException('Need a local container')
+        raise WildlandError('Need a local container')
 
     container_mount_path = container.paths[0]
     click.echo(f'Using container: {container.local_path} ({container_mount_path})')
@@ -136,10 +135,7 @@ def _do_create(
         }
 
     if access:
-        try:
-            access = [{'user': obj.client.load_user_by_name(user).owner} for user in access]
-        except WildlandError as ex:
-            raise CliError(f'Failed to create storage: {ex}') from ex
+        access = [{'user': obj.client.load_user_by_name(user).owner} for user in access]
     else:
         if container.access:
             access = container.access
@@ -155,10 +151,7 @@ def _do_create(
         manifest_pattern=params.get('manifest_pattern', manifest_pattern_dict),
         access=access
     )
-    try:
-        storage.validate()
-    except SchemaError as se:
-        raise CliError(f'Invalid storage properties: {se}') from se
+    storage.validate()
 
     _do_save_new_storage(obj.client, container, storage, inline, name)
 
@@ -257,7 +250,7 @@ def delete(obj: ContextObj, name, force, cascade):
         return
 
     if not storage.local_path:
-        raise CliError('Can only delete a local manifest')
+        raise WildlandError('Can only delete a local manifest')
 
     used_by = []
     for container in obj.client.load_containers():
@@ -351,13 +344,13 @@ def create_from_set(obj: ContextObj, cont, storage_set=None, local_dir=None):
         storage_set = obj.client.config.get('default-storage-set-for-user')\
             .get(container.owner, None)
         if not storage_set:
-            raise CliError(f'User {container.owner} has no default storage template set. '
-                           f'Specify template set explicitly.')
+            raise WildlandError(f'User {container.owner} has no default storage template set. '
+                                f'Specify template set explicitly.')
 
     try:
         storage_set = template_manager.get_storage_set(storage_set)
     except FileNotFoundError as fnf:
-        raise CliError(f'Storage set {storage_set} not found.') from fnf
+        raise WildlandError(f'Storage set {storage_set} not found.') from fnf
 
     try:
         do_create_storage_from_set(obj.client, container, storage_set, local_dir)

wildland/cli/cli_storage_set.py
@@ -23,7 +23,8 @@ Storage set management
 import click
 import yaml
 
-from .cli_base import aliased_group, ContextObj, CliError
+from .cli_base import aliased_group, ContextObj
+from ..exc import WildlandError
 from ..manifest.manifest import ManifestError
 from ..manifest.template import TemplateManager, SET_SUFFIX
 
@@ -107,7 +108,7 @@ def set_del_(obj: ContextObj, name):
     try:
         removed_path = template_manager.remove_storage_set(name)
     except FileNotFoundError as fnf:
-        raise CliError(f'template set {name} not found.') from fnf
+        raise WildlandError(f'template set {name} not found.') from fnf
 
     click.echo(f'Deleted storage template set {removed_path}.')
 
@@ -126,13 +127,13 @@ def set_default_(obj: ContextObj, user, name):
     try:
         template_manager.get_storage_set(name)
     except FileNotFoundError as fnf:
-        raise CliError(f'Storage set {name} does not exist') from fnf
+        raise WildlandError(f'Storage set {name} does not exist') from fnf
 
     obj.client.recognize_users()
     try:
         user = obj.client.load_user_by_name(user_name)
     except ManifestError as ex:
-        raise CliError(f'User {user_name} load failed: {ex}') from ex
+        raise WildlandError(f'User {user_name} load failed: {ex}') from ex
 
     default_sets = obj.client.config.get('default-storage-set-for-user')
     default_sets[user.owner] = name
@@ -163,7 +164,7 @@ def add_template(obj: ContextObj, storage_set, template, inline):
     try:
         storage_set = template_manager.get_storage_set(storage_set)
     except FileNotFoundError as fnf:
-        raise CliError(f'Template set \'{storage_set}\' not found.') from fnf
+        raise WildlandError(f'Template set \'{storage_set}\' not found.') from fnf
 
     templates_to_add = [(t, 'file') for t in template] + [(t, 'inline') for t in inline]
 
@@ -171,7 +172,7 @@ def add_template(obj: ContextObj, storage_set, template, inline):
         try:
             storage_set.add_template(template_name, template_type)
         except FileNotFoundError as fnf:
-            raise CliError(f'Template file {template_name} not found.') from fnf
+            raise WildlandError(f'Template file {template_name} not found.') from fnf
 
     click.echo(f'Saving modified storage set {storage_set.name} to {storage_set.path}.')
     storage_set.path.write_text(yaml.dump(storage_set.to_dict()))
@@ -190,13 +191,13 @@ def del_template(obj: ContextObj, storage_set, template):
     try:
         storage_set = template_manager.get_storage_set(storage_set)
     except FileNotFoundError as fnf:
-        raise CliError(f'Template set \'{storage_set}\' not found.') from fnf
+        raise WildlandError(f'Template set \'{storage_set}\' not found.') from fnf
 
     for t in template:
         try:
             storage_set.remove_template(t)
         except FileNotFoundError as fnf:
-            raise CliError(f'Template file {t} not found.') from fnf
+            raise WildlandError(f'Template file {t} not found.') from fnf
 
     click.echo(f'Saving modified storage set {storage_set.name} to {storage_set.path}.')
     storage_set.path.write_text(yaml.dump(storage_set.to_dict()))

commit ae0904066fe6c9e8083ddf4d71d77f93e7f8a443

Fix `wl --help`
This commit originally was intended to address the problem with printing
backtrace when Wildland's config.yaml contains the following invalid
configuration (as described in issue #245):

  default-containers:
  # - 'this line is commented out'

This issue was addressed in commit c31cbc4 though (latest origin/master
at the time of writing). The problem is that commit c31cbc4 introduced
the bug which causes `wl --help` to print `Error: 'debug'` instead of a
help section. This commit fixes the problem in `wl --help` handling.

Fixes #245

wildland/cli/cli_base.py
@@ -97,7 +97,7 @@ class AliasedGroup(click.Group):
         self.aliases.update(kwds)
 
     def get_command(self, ctx, cmd_name):
-        if self.name == 'wl':
+        if self.name == 'wl' and 'debug' in ctx.params:
             self.debug = ctx.params['debug']
 
         # 1) try exact command

wildland/tests/test_cli.py
@@ -3060,3 +3060,19 @@ def test_user_refresh(cli, base_dir, tmpdir):
 
     user_data = (base_dir / 'users/Alice.user.yaml').read_text()
     assert 'paths:\n- /MEH' in user_data
+
+
+## Global options (--help, --version etc.)
+
+
+def test_wl_help(cli):
+    result = cli('--help', capture=True)
+    assert 'Usage:' in result
+    assert 'Options:' in result
+    assert 'Commands:' in result
+    assert 'Aliases:' in result
+
+
+def test_wl_version(cli):
+    result = cli('--version', capture=True)
+    assert 'version' in result