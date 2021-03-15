b6c3ca8e264023eda12c6d4665d19a7e22eb8e2a
Wojtek Porczyk <woju@invisiblethingslab.com>
2021-03-15 19:32:59 +0100

    publish: unbpublish from all infrastructures

--- wildland/publish.py
+++ wildland/publish.py
@@ -24,7 +24,7 @@ Stuff related to publishing and unpublishing containers.
 import collections.abc
 import logging
 import pathlib
-from typing import Optional, cast
+from typing import Optional, Generator, cast
 
 from .client import Client
 from .container import Container
@@ -60,10 +60,119 @@ class Publisher:
             raise NotImplementedError(
                 'choosing infrastructure is not supported')
 
-        self.infra_storage = next(self._get_storages_for_publish())
+        self.container_uuid_path = self.container.get_uuid_path()
+
+    def publish_container(self) -> None:
+        """
+        Publish the manifest
+        """
+        _StoragePublisher(self, next(self._get_storages_for_publish())
+            ).publish_container(False)
+
+    def unpublish_container(self) -> None:
+        """
+        Unpublish the manifest
+        """
+        for storage in self._get_storages_for_publish():
+            _StoragePublisher(self, storage).publish_container(True)
+
+    def _get_storages_for_publish(self) -> Generator[Storage, None, None]:
+        '''
+        Iterate over all suitable storages to publish container manifest.
+        '''
+        owner = self.client.load_user_by_name(self.container.owner)
+
+        ok = False
+        rejected = []
+        if not owner.containers:
+            rejected.append(f'user {owner} has no infrastructure containers')
+
+        for c in owner.containers:
+            try:
+                container_candidate = (
+                    self.client.load_container_from_url_or_dict(
+                        c, self.container.owner))
+
+                all_storages = list(
+                    self.client.all_storages(container=container_candidate))
+
+                if not all_storages:
+                    rejected.append(
+                        f'container {container_candidate.ensure_uuid()} '
+                        'has no available storages')
+                    continue
+
+                for storage_candidate in all_storages:
+                    if storage_candidate.manifest_pattern is None:
+                        rejected.append(
+                            f'storage {storage_candidate.params["backend-id"]} of '
+                            f'container {container_candidate.ensure_uuid()} '
+                            'does not have manifest_pattern')
+                        continue
+
+                    if not storage_candidate.is_writeable:
+                        rejected.append(
+                            f'storage {storage_candidate.params["backend-id"]} of '
+                            f'container {container_candidate.ensure_uuid()} '
+                            'is not writeable')
+                        continue
+
+                    # Attempt to mount the storage driver first.
+                    # Failure in attempt to mount the backend should try the next storage from the
+                    # container and if still not mounted, move to the next container
+                    try:
+                        with StorageDriver.from_storage(storage_candidate) as _driver:
+                            ok = True
+                            yield storage_candidate
+
+                            # yield at most a single storage for a container
+                            break
+
+                    except (WildlandError, PermissionError, FileNotFoundError) as ex:
+                        rejected.append(
+                            f'storage {storage_candidate.params["backend-id"]} of '
+                            f'container {container_candidate.ensure_uuid()} '
+                            f'could not be mounted: {ex!s}')
+                        logger.debug(
+                            'Failed to mount storage when publishing with '
+                            'exception: %s',
+                            ex)
+                        continue
+
+            except (ManifestError, WildlandError) as ex:
+                rejected.append(
+                    f'container {container_candidate.ensure_uuid()} has '
+                    'serious problems: {ex!s}')
+                logger.debug(
+                    'Failed to load container when publishing with '
+                    'exception: %s',
+                    ex)
+                continue
+
+        if not ok:
+            raise WildlandError(
+                'Cannot find any container suitable as publishing platform:'
+                + ''.join(f'\n- {i}' for i in rejected))
+
+
+class _StoragePublisher:
+    """
+    Helper class: publish/unpublish for a single storage
+
+    This is because publishing is done to single storage, but unpublish should
+    be attempted from all viable infra containers to avoid a situation when user
+    commands an unpublish, we find no manifests at some container and report to
+    user that there no manifests, which would obviously be wrong.
+    """
+
+    def __init__(self, publisher: Publisher, infra_storage: Storage):
+        self.client = publisher.client
+        self.container = publisher.container
+        self.container_uuid_path = publisher.container_uuid_path
+
+        self.infra_storage = infra_storage
         assert self.infra_storage.manifest_pattern['type'] == 'glob'
         self.pattern = self.infra_storage.manifest_pattern['path']
-        self.container_uuid_path = self.container.get_uuid_path()
 
     def _get_relpath_for_storage_manifest(self, storage):
         # we publish only a single manifest for a storage, under `/.uuid/` path
@@ -87,20 +196,7 @@ class Publisher:
                 yield pathlib.PurePosixPath(path_pattern.replace(
                     '{path}', str(path.relative_to('/')))).relative_to('/')
 
-
-    def publish_container(self) -> None:
-        """
-        Publish the manifest
-        """
-        return self._publish_container(False)
-
-    def unpublish_container(self) -> None:
-        """
-        Unpublish the manifest
-        """
-        return self._publish_container(True)
-
-    def _publish_container(self, just_unpublish: bool) -> None:
+    def publish_container(self, just_unpublish: bool) -> None:
         """
         Publish a container to a container owner by the same user.
         """
@@ -189,81 +285,3 @@ class Publisher:
                     driver.makedirs(relpath.parent)
                     driver.write_file(relpath,
                         self.client.session.dump_container(self.container))
-
-    def _get_storages_for_publish(self) -> Storage:
-        '''
-        Iterate over all suitable storages to publish container manifest.
-        '''
-        owner = self.client.load_user_by_name(self.container.owner)
-
-        ok = False
-        rejected = []
-        if not owner.containers:
-            rejected.append(f'user {owner} has no infrastructure containers')
-
-        for c in owner.containers:
-            try:
-                container_candidate = (
-                    self.client.load_container_from_url_or_dict(
-                        c, self.container.owner))
-
-                all_storages = list(
-                    self.client.all_storages(container=container_candidate))
-
-                if not all_storages:
-                    rejected.append(
-                        f'container {container_candidate.ensure_uuid()} '
-                        'has no available storages')
-                    continue
-
-                for storage_candidate in all_storages:
-                    if storage_candidate.manifest_pattern is None:
-                        rejected.append(
-                            f'storage {storage_candidate.params["backend-id"]} of '
-                            f'container {container_candidate.ensure_uuid()} '
-                            'does not have manifest_pattern')
-                        continue
-
-                    if not storage_candidate.is_writeable:
-                        rejected.append(
-                            f'storage {storage_candidate.params["backend-id"]} of '
-                            f'container {container_candidate.ensure_uuid()} '
-                            'is not writeable')
-                        continue
-
-                    # Attempt to mount the storage driver first.
-                    # Failure in attempt to mount the backend should try the next storage from the
-                    # container and if still not mounted, move to the next container
-                    try:
-                        with StorageDriver.from_storage(storage_candidate) as _driver:
-                            ok = True
-                            yield storage_candidate
-
-                            # yield at most a single storage for a container
-                            break
-
-                    except (WildlandError, PermissionError, FileNotFoundError) as ex:
-                        rejected.append(
-                            f'storage {storage_candidate.params["backend-id"]} of '
-                            f'container {container_candidate.ensure_uuid()} '
-                            f'could not be mounted: {ex!s}')
-                        logger.debug(
-                            'Failed to mount storage when publishing with '
-                            'exception: %s',
-                            ex)
-                        continue
-
-            except (ManifestError, WildlandError) as ex:
-                rejected.append(
-                    f'container {container_candidate.ensure_uuid()} has '
-                    'serious problems: {ex!s}')
-                logger.debug(
-                    'Failed to load container when publishing with '
-                    'exception: %s',
-                    ex)
-                continue
-
-        if not ok:
-            raise WildlandError(
-                'Cannot find any container suitable as publishing platform:'
-                + ''.join(f'\n- {i}' for i in rejected))
