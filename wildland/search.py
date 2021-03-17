golemfoundation/wildland-client

commit def5696db78211f6f3679d9c2fea963a8ebb4a3f

Avoid client<->search cyclic import issue
search imports client only for type annotations, so make the import
conditional on type checking enabled. And also enable postponed
evaluation of annotations to make it work - see PEP 563.

wildland/search.py
@@ -21,6 +21,8 @@
 Utilities for URL resolving and traversing the path
 '''
 
+from __future__ import annotations
+
 import errno
 import logging
 import os
@@ -29,9 +31,9 @@ import types
 from dataclasses import dataclass
 from pathlib import PurePosixPath
 from typing import Optional, Tuple, Iterable, Mapping
+from typing import TYPE_CHECKING
 
 from .user import User
-from .client import Client
 from .container import Container
 from .bridge import Bridge
 from .storage import Storage
@@ -40,6 +42,8 @@ from .manifest.manifest import ManifestError
 from .wlpath import WildlandPath, PathError
 from .exc import WildlandError
 
+if TYPE_CHECKING:
+    from .client import Client
 
 logger = logging.getLogger('search')
 

wildland/client.py
@@ -45,6 +45,7 @@ from .storage_backends.base import StorageBackend, verify_local_access
 from .fs_client import WildlandFSClient
 from .config import Config
 from .exc import WildlandError
+from .search import Search
 
 logger = logging.getLogger('client')
 
@@ -254,13 +255,9 @@ class Client:
             trusted_owner=trusted_owner)
 
     def load_container_from_wlpath(self, wlpath: WildlandPath) -> Iterable[Container]:
-        '''
+        """
         Load containers referring to a given WildlandPath.
-        '''
-
-        # TODO: Still a circular dependency with search
-        # pylint: disable=import-outside-toplevel, cyclic-import
-        from .search import Search
+        """
         search = Search(self, wlpath, self.config.aliases)
         yield from search.read_container()
 
@@ -272,9 +269,6 @@ class Client:
         if WildlandPath.match(url):
             wlpath = WildlandPath.from_str(url)
             if wlpath.file_path is None:
-                # TODO: Still a circular dependency with search
-                # pylint: disable=import-outside-toplevel, cyclic-import
-                from .search import Search
 
                 search = Search(self, wlpath, {'default': owner})
                 try:
@@ -790,9 +784,6 @@ class Client:
         if not wlpath.owner and not use_aliases:
             raise WildlandError(
                 'Wildland path in URL context has to have explicit owner')
-        # TODO: Still a circular dependency with search
-        # pylint: disable=import-outside-toplevel
-        from .search import Search
 
         search = Search(self, wlpath,
                         self.config.aliases if use_aliases else {})

commit 92bab0c133d66271bd54362820f1e33f254fe111

search: ignore cyclic import also in pylint
I'm not sure why this wasn't caught earlier, and it interferes with
unrelated MR.

Fixes: def5696db78211f6f3679d9c2fea963a8ebb4a3f

wildland/search.py
@@ -44,7 +44,7 @@ from .wlpath import WildlandPath, PathError
 from .exc import WildlandError
 
 if TYPE_CHECKING:
-    from .client import Client
+    from .client import Client # pylint: disable=cyclic-import
 
 logger = logging.getLogger('search')
 