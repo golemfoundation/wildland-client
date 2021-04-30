3c4644920e80c385ae5abe3ec9bbb2be94af9817
Paul Peregud <paulperegud@protonmail.ch>
2021-04-30 13:23:09 +0200

    WL-205: test: use pidof, xargs and kill instead of killall
    parametrize the test instead of running two similar copies

--- wildland/tests/test_encrypted.py
+++ wildland/tests/test_encrypted.py
@@ -36,7 +36,8 @@ from ..cli.cli_base import ContextObj
 from ..cli.cli_main import _do_mount_containers
 from ..manifest.manifest import WildlandObjectType
 
-def test_encrypted_with_url_and_gocryptfs(cli, base_dir):
+@pytest.mark.parametrize('engine', ['gocryptfs', 'encfs'])
+def test_encrypted_with_url(cli, base_dir, engine):
     local_dir = base_dir / 'local'
     Path(local_dir).mkdir()
     cli('user', 'create', 'User', '--key', '0xaaa')
@@ -52,7 +53,7 @@ def test_encrypted_with_url_and_gocryptfs(cli, base_dir):
     cli('container', 'create', 'Container', '--path', '/PATH')
     cli('storage', 'create', 'encrypted', 'ProxyStorage',
         '--reference-container-url', reference_url,
-        '--engine', 'gocryptfs',
+        '--engine', engine,
         '--container', 'Container', '--inline')
 
     client = Client(base_dir)
@@ -65,7 +66,7 @@ def test_encrypted_with_url_and_gocryptfs(cli, base_dir):
     storage = client.select_storage(container)
     assert storage.storage_type == 'encrypted'
     assert storage.params['symmetrickey']
-    assert storage.params['engine'] in ['gocryptfs', 'cryfs', 'encfs']
+    assert storage.params['engine'] in ['gocryptfs', 'encfs']
     reference_storage = storage.params['storage']
     assert isinstance(reference_storage, dict)
     assert reference_storage['type'] == 'local'
@@ -84,75 +85,7 @@ def test_encrypted_with_url_and_gocryptfs(cli, base_dir):
 
     assert os.listdir(mounted_plaintext) == ['test.file']
 
-    time.sleep(1) # time to let gocryptfs finish writing to plaintext dir
-
-    # check if ciphertext directory looks familiar
-    listing = os.listdir(local_dir)
-    assert len(listing) == 3
-    assert 'gocryptfs.conf' in listing
-    assert 'gocryptfs.diriv' in listing
-    listing.remove('gocryptfs.conf')
-    listing.remove('gocryptfs.diriv')
-
-    # read and examine entropy of ciphertext file
-    with open(local_dir / listing[0], 'rb') as fb:
-        enc_bytes = fb.read()
-    packed_bytes = zlib.compress(enc_bytes)
-    assert len(packed_bytes) * 1.05 > len(enc_bytes), "encrypted bytes are of low entropy!"
-
-    time.sleep(1) # otherwise "unmount: /tmp/.../mnt: target is busy"
-
-def test_encrypted_with_url_and_encfs(cli, base_dir):
-    local_dir = base_dir / 'local'
-    Path(local_dir).mkdir()
-    cli('user', 'create', 'User', '--key', '0xaaa')
-    cli('container', 'create', 'referenceContainer', '--path', '/reference_PATH')
-    cli('storage', 'create', 'local', 'referenceStorage', '--location', local_dir,
-        '--container', 'referenceContainer', '--no-inline')
-
-    reference_path = base_dir / 'containers/referenceContainer.container.yaml'
-    assert reference_path.exists()
-    # handle both files and paths here
-    reference_url = 'wildland::/reference_PATH:'
-
-    cli('container', 'create', 'Container', '--path', '/PATH')
-    cli('storage', 'create', 'encrypted', 'ProxyStorage',
-        '--reference-container-url', reference_url,
-        '--engine', 'encfs',
-        '--container', 'Container', '--inline')
-
-    client = Client(base_dir)
-    client.recognize_users()
-
-    obj = ContextObj(client)
-    obj.fs_client = client.fs_client
-
-    # But select_storage loads also the reference manifest
-    container = client.load_object_from_name(WildlandObjectType.CONTAINER, 'Container')
-    storage = client.select_storage(container)
-    assert storage.storage_type == 'encrypted'
-    assert storage.params['symmetrickey']
-    assert storage.params['engine'] in ['gocryptfs', 'cryfs', 'encfs']
-    reference_storage = storage.params['storage']
-    assert isinstance(reference_storage, dict)
-    assert reference_storage['type'] == 'local'
-
-    # start and check if engine is running
-    user = client.users['0xaaa']
-    client.fs_client.mount(single_thread=False, default_user=user)
-    to_mount = ['referenceContainer', 'Container']
-    _do_mount_containers(obj, to_mount)
-    subprocess.run(['pidof', 'encfs'], check=True)
-
-    # write and read a file
-    mounted_plaintext = obj.fs_client.mount_dir / Path('/PATH').relative_to('/')
-    assert os.listdir(mounted_plaintext) == [], "plaintext dir should be empty!"
-    with open(mounted_plaintext / 'test.file', 'w') as ft:
-        ft.write("1" * 10000) # low entropy plaintext file
-
-    assert os.listdir(mounted_plaintext) == ['test.file']
-
-    time.sleep(1) # time to let gocryptfs finish writing to plaintext dir
+    time.sleep(1) # time to let engine finish writing to plaintext dir
 
     # check if ciphertext directory looks familiar
     listing = os.listdir(local_dir)
@@ -170,13 +103,13 @@ def test_encrypted_with_url_and_encfs(cli, base_dir):
     ft2 = open(mounted_plaintext / 'leak.test', 'w')
 
     if engine == 'gocryptfs':
-        subprocess.run(['killall', engine], check=True)
+        subprocess.run(['pidof gocryptfs | xargs kill'], shell=True, check=True)
         time.sleep(1)
         with pytest.raises(FileNotFoundError):
             ft2.write("2" * 10000)
 
     if engine == 'encfs':
-        subprocess.run(['killall', '-9', engine], check=True)
+        subprocess.run(['pidof encfs | xargs kill -9'], shell=True, check=True)
         time.sleep(1)
         with pytest.raises(OSError):
             ft2.write("2" * 10000)
