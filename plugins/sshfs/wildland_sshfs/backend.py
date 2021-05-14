1c9e85973906f75dee2183e34c349356ed98af48
Piotr K. Isajew <pki@ex.com.pl>
2021-05-14 14:39:57 +0200

    an example sshfs implementation

---
+++ plugins/sshfs/wildland_sshfs/backend.py
@@ -0,0 +1,133 @@
+"""
+Wildland storage backend using sshfs
+"""
+
+import logging
+import click
+import os
+
+from typing import Tuple
+from pathlib import PurePosixPath, Path
+from subprocess import Popen, PIPE, run
+from .local_proxy import LocalProxy
+
+from wildland.storage_backends.base import StorageBackend
+
+logger = logging.getLogger('storage-sshfs')
+
+
+class SshFsBackend(LocalProxy):
+    """
+    sshfs backend
+    """
+
+    TYPE='sshfs'
+
+    def __init__(self, **kwds):
+        super().__init__(**kwds)
+        
+        self.sshfs_cmd = kwds['params']['cmd']
+        self.inner_mount_point = None
+        self.sshfs_host = kwds['params']['host']
+        self.sshfs_path = kwds['params']['path']
+        self.mount_opts = kwds['params'].get('mount_opts')
+        self.login = kwds['params']['login']
+        self.passwd = kwds['params'].get('passwd')
+        self.identity = kwds['params'].get('identity')
+
+    ### Abstract method implementations
+    def unmount_inner_fs(self, path: PurePosixPath) -> None:
+        cmd = ["umount", str(path)]
+        logger.debug(f"executing {cmd}")
+        res = run(cmd, stderr=PIPE)
+        if res.returncode != 0:
+            logger.error("unable to unmount sshfs (%d)", 
+                         res.returncode)
+        if len(res.stderr) > 0:
+            logger.error(res.stderr.decode())
+
+
+    def mount_inner_fs(self, path: PurePosixPath) -> None:
+        cmd = [self.sshfs_cmd]
+        if self.passwd:
+            cmd.extend(['-o', 'password_stdin'])
+        if self.identity:
+            ipath = self.backend_dir() / '.identity'
+            with open(ipath, 'w') as of: 
+                of.write(self.identity)
+                cmd.extend(['-o', f'IdentityFile={ipath}'])
+
+        if self.mount_opts: cmd.append(self.mount_opts)
+
+        addr = self.sshfs_host
+        if self.sshfs_path: addr += ':' + self.sshfs_path
+        if self.login: addr = self.login + '@' + addr
+
+        cmd.append(addr)
+        cmd.append(str(path))
+
+        logger.debug(f"will execute: {cmd}")
+        executor = Popen(cmd, stdout=PIPE, stdin=PIPE)
+        out, _ = executor.communicate(bytes(self.passwd, 'utf-8'))
+
+        if executor.returncode != 0:
+            logger.error("Failed to mount sshfs filesystem (%d)", 
+                         sp.returncode)
+            if len(out.decode()) > 0:
+                logger.error(out.decode())
+            raise WildlandFSError("unable to mount sshfs")
+    
+    @classmethod
+    def cli_create(cls, data):
+
+        if data['ssh_identity'] and data['pwprompt']:
+            raise click.UsageError('pwprompt and ssh-identity are mutually exclusive')
+        conf = {
+            'cmd': data['sshfs_command'],
+            'login': data['ssh_user'],
+            'mount_opts': data['mount_options'],
+            'host': data['host'],
+            'path': data['path']
+        }
+
+
+        if data['pwprompt']:
+            conf['passwd'] = click.prompt('SSH password', 
+                                          hide_input=True)
+        
+        if data['ssh_identity']:
+            with open(data['ssh_identity']) as f:
+                conf['identity'] = f.readlines()
+        return conf
+
+    @classmethod
+    def cli_options(cls):
+        return [
+            click.Option(['--sshfs-command'],
+                         default='sshfs',
+                         metavar='CMD',
+                         required=True,
+                         show_default=True,
+                         help='command to mount sshfs filesystem',
+                         ),
+            click.Option(['--host'],
+                         required=True,
+                         metavar='HOST',
+                         help='host to mount',
+                         ),
+            click.Option(['--path'],
+                         help='path on target host to mount'),
+            click.Option(['--ssh-user'],
+                         metavar='USER',
+                         help='user name to log on to target host',
+                         ),
+            click.Option(['--ssh-identity'],
+                         metavar='PATH',
+                         help='path to private key file to use for authentication',
+                         ),
+            click.Option(['--pwprompt'], is_flag=True,
+                         help='prompt for password that will be used for authentication'),
+            click.Option(['--mount-options'],
+                         metavar='OPT1,OPT2,...',
+                         help='additional options to be passed to sshfs command directly'),
+        ]
---
+++ plugins/sshfs/wildland_sshfs/local_proxy.py
@@ -0,0 +1,115 @@
+# Wildland Project
+#
+# The contents of this file is primarily inspired by
+# Pawel Peregud's work on encrypted backend.
+
+import abc
+import logging
+import secrets
+import string
+from os import rmdir
+from tempfile import mkdtemp
+from pathlib import PurePosixPath, Path
+from wildland.storage_backends.base import StorageBackend, File
+from wildland.storage_backends.local import LocalStorageBackend
+from wildland.wlenv import WLEnv
+
+logger = logging.getLogger('local-proxy')
+
+class LocalProxy(StorageBackend):
+    """
+    An abstract base class for implementation of proxy backends
+    which expose exposing locally mounted filesystems
+    as Wildland storage.
+    """
+
+    def __init__(self, **kwds):
+        super().__init__(**kwds)
+        self.inner_mount_point = None
+        self.local = None
+        self.owner = kwds['params']['owner']
+
+    def open(self, path: PurePosixPath, flags: int) -> File:
+        return self.inner_backend.open(path, flags)
+
+    def backend_dir(self) -> Path:
+        return WLEnv().temp_root() / 'wllpb' / self.backend_id
+
+    def mount(self):
+        """
+        mount the file system
+        """
+        # Ensure mount point for inner file system
+        alphabet = string.ascii_letters + string.digits
+        mountid  = ''.join(secrets.choice(alphabet) for i in range(15))
+        self.inner_mount_point = self.backend_dir() / mountid
+        Path(self.inner_mount_point).mkdir(parents=True)
+        
+
+        backend_params = { 'location': self.inner_mount_point,
+                           'type': 'local',
+                           'owner': self.owner,
+                           'is-local-owner': True,
+                           'backend-id': mountid + '/inner'
+                          }
+        self.local = LocalStorageBackend(params=backend_params)
+        
+        # and do actually mount it
+        self.mount_inner_fs(self.inner_mount_point)
+
+        self.local.request_mount()
+        logger.debug("inner file system mounted at: %s", 
+                     self.inner_mount_point)
+
+    def unmount(self):
+        """
+        unmount the file system
+        """
+        logger.debug("will unmount inner filesystem at: %s",
+                     self.inner_mount_point)
+        self.local.request_unmount()
+        self.unmount_inner_fs(self.inner_mount_point)
+        rmdir(self.inner_mount_point)
+
+
+    @abc.abstractmethod
+    def mount_inner_fs(self, path: PurePosixPath) -> None:
+        """
+        Called to mount inner filesystem at given path.
+        """
+
+    @abc.abstractmethod
+    def unmount_inner_fs(self, path: PurePosixPath) -> None:
+        """
+        Called to unmount inner filesystem.
+        """
+
+    def getattr(self, path: PurePosixPath):
+        return self.local.getattr(path)
+
+    def readdir(self, path: PurePosixPath):
+        return self.local.readdir(path)
+
+    def truncate(self, path: PurePosixPath, length: int) -> None:
+        return self.local.truncate(path, length)
+
+    def unlink(self, path: PurePosixPath):
+        return self.local.unlink(path)
+
+    def mkdir(self, path: PurePosixPath, mode: int = 0o777) -> None:
+        return self.local.mkdir(path, mode)
+
+    def rmdir(self, path: PurePosixPath) -> None:
+        return self.local.rmdir(path)
+
+    def chmod(self, path: PurePosixPath, mode: int) -> None:
+        return self.local.chmod(path, mode)
+
+    def chown(self, path: PurePosixPath, uid: int, gid: int) -> None:
+        return self.local.chown(path, uid, gid)
+
+    def rename(self, move_from: PurePosixPath, move_to: PurePosixPath):
+        return self.local.rename(move_from, move_to)
+
+    def utimens(self, path: PurePosixPath, atime, mtime) -> None:
+        return self.local.utimens(path, atime, mtime)
