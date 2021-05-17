9bb475b35745b0ba65385b0b34f3837ea11667a7
Piotr K. Isajew <pki@ex.com.pl>
2021-05-17 12:47:04 +0200

    addressing pylint warnings

--- wildland/wlenv.py
+++ wildland/wlenv.py
@@ -1,6 +1,6 @@
 """
 At some point this should provide OS-independent
-environment information (i.e. config location, 
+environment information (i.e. config location,
 temporary storage directory, etc.) for internal
 wildland usage.
 """
@@ -9,14 +9,24 @@ import os
 from pathlib import PurePosixPath, Path
 
 class WLEnv:
+    """
+    WLEnv class provides information to parts of Wildland
+    that need to interact with the OS environment in system
+    independent way.
+    """
+
+    def __init__(self):
+        super().__init__()
+        default = Path('~/.local/share/').expanduser()
+        self._storage_dir = PurePosixPath(os.getenv('XDG_HOME_DATA',
+                                                    default)) / 'wl'
 
     def storage_dir(self) -> PurePosixPath:
         """
         returns path to root directory of wildland dedicated storage
         area.
         """
-        default = Path('~/.local/share/').expanduser()
-        return PurePosixPath(os.getenv('XDG_HOME_DATA', default)) / 'wl'
+        return self._storage_dir
 
     def temp_root(self) -> PurePosixPath:
         """
