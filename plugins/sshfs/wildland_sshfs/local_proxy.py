54bd671f6dbda3a5a11b75ad98d437bc2db6fd70
Piotr K. Isajew <pki@ex.com.pl>
2021-05-17 15:16:23 +0200

    silence mypy

--- plugins/sshfs/wildland_sshfs/local_proxy.py
+++ plugins/sshfs/wildland_sshfs/local_proxy.py
@@ -12,6 +12,7 @@ import logging
 import secrets
 import string
 from os import rmdir
+from typing import Optional
 from pathlib import PurePosixPath, Path
 from wildland.storage_backends.base import StorageBackend, File
 from wildland.storage_backends.local import LocalStorageBackend
@@ -28,11 +29,12 @@ class LocalProxy(StorageBackend):
 
     def __init__(self, **kwds):
         super().__init__(**kwds)
-        self.inner_mount_point = None
-        self.local = None
+        self.inner_mount_point: Optional[PurePosixPath] = None
+        self.local: Optional[StorageBackend] = None
         self.owner = kwds['params']['owner']
 
     def open(self, path: PurePosixPath, flags: int) -> File:
+        assert self.local
         return self.local.open(path, flags)
 
     def backend_dir(self) -> Path:
@@ -49,7 +51,7 @@ class LocalProxy(StorageBackend):
         # Ensure mount point for inner file system
         alphabet = string.ascii_letters + string.digits
         mountid  = ''.join(secrets.choice(alphabet) for i in range(15))
-        self.inner_mount_point = self.backend_dir() / mountid
+        self.inner_mount_point = PurePosixPath(self.backend_dir()) / mountid
         Path(self.inner_mount_point).mkdir(parents=True)
 
 
@@ -72,8 +74,10 @@ class LocalProxy(StorageBackend):
         """
         unmount the file system
         """
+        assert self.inner_mount_point
         logger.debug("will unmount inner filesystem at: %s",
                      self.inner_mount_point)
+        assert self.local
         self.local.request_unmount()
         self.unmount_inner_fs(self.inner_mount_point)
         rmdir(self.inner_mount_point)
@@ -92,31 +96,41 @@ class LocalProxy(StorageBackend):
         """
 
     def getattr(self, path: PurePosixPath):
+        assert self.local
         return self.local.getattr(path)
 
     def readdir(self, path: PurePosixPath):
+        assert self.local
         return self.local.readdir(path)
 
     def truncate(self, path: PurePosixPath, length: int) -> None:
+        assert self.local
         return self.local.truncate(path, length)
 
     def unlink(self, path: PurePosixPath):
+        assert self.local
         return self.local.unlink(path)
 
     def mkdir(self, path: PurePosixPath, mode: int = 0o777) -> None:
+        assert self.local
         return self.local.mkdir(path, mode)
 
     def rmdir(self, path: PurePosixPath) -> None:
+        assert self.local
         return self.local.rmdir(path)
 
     def chmod(self, path: PurePosixPath, mode: int) -> None:
+        assert self.local
         return self.local.chmod(path, mode)
 
     def chown(self, path: PurePosixPath, uid: int, gid: int) -> None:
+        assert self.local
         return self.local.chown(path, uid, gid)
 
     def rename(self, move_from: PurePosixPath, move_to: PurePosixPath):
+        assert self.local
         return self.local.rename(move_from, move_to)
 
     def utimens(self, path: PurePosixPath, atime, mtime) -> None:
+        assert self.local
         return self.local.utimens(path, atime, mtime)
