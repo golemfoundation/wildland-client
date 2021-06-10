7b6dd25668676cc0a3b645e6fff8b6dcbabb1915
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2021-06-10 18:36:13 +0200

    howto-test-lib: support PCRE for matching expected output

--- ci/howto-test-lib.bash
+++ ci/howto-test-lib.bash
@@ -12,6 +12,17 @@
 #   second line of output"
 #   run wl user dump test
 #
+# For more complex cases, 'expected_pcre' can be used. It will compare the
+# output against a PCRE, in a multi-line mode. Note, the '.' normally matches a
+# single character only _except newline_. To create a matching any number of
+# lines, start the expression with "(?s)" - this will make '.' to match also a
+# newline. Non-newline character can still be matched using \N. See
+# pcrepattern(3) for more details. Example:
+#   expected_pcre="(?s)Some output, then any number of lines
+#   .*
+#   and finally the last line"
+#   run wl user dump test
+#
 
 set -eo pipefail
 
@@ -38,6 +49,16 @@ run() {
     if ! actual=$("$@"); then
         printf '%s\n%s-> FAILED%s\n' "$actual" "$red$bold" "$norm"
         exit 1
+    fi
+    if [ "${expected_pcre-x}" != x ]; then
+        expected_pcre=${expected_pcre//
+/\\n}
+        if ! grep -Pzo -- "$expected_pcre" <(printf '%s' "$actual"); then
+            printf '%s\n\n' "$actual"
+            printf '%s-> OUTPUT DOES NOT MATCH%s\n' "$red$bold" "$norm"
+            exit 1
+        fi
+        unset expected_pcre
     fi
     if [ "${expected-x}" != x ]; then
         if ! diff -u <(printf '%s' "$expected") <(printf '%s' "$actual"); then
