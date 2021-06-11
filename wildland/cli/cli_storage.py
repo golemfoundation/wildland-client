4635841c3f7f40c4a3c2fde87ec645d18c8d79a1
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2021-06-11 21:08:35 +0200

    Revert "storage-template: don't crash on proxy storages"
    This reverts commit 69c4cba277601552e7cf9663bb19c277e5a6fc68.

--- wildland/cli/cli_storage.py
+++ wildland/cli/cli_storage.py
@@ -359,11 +359,7 @@ def do_create_storage_from_templates(
 
             storage.params[storage_cls.LOCATION_PARAM] = str(location)
 
-            if 'reference-container' in storage.params:
-                click.echo(
-                    f'WARN: Will not attempt to create a directory in a {storage_type} storage')
-            else:
-                backend = StorageBackend.from_params(storage.params)
+            backend = StorageBackend.from_params(storage.params)
 
         to_process.append((storage, backend))
 
