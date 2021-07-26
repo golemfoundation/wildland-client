b1ff7148bce65720ee9f5ffc91d20f47095c8cf2
Piotr Bartman <prbartman@invisiblethingslab.com>
2021-07-26 10:19:17 +0200

    fixed type hinting in StorageBackend
    using 'from __future__ import annotations' to handle cyclic-import Client

--- wildland/storage_backends/base.py
+++ wildland/storage_backends/base.py
@@ -25,6 +25,7 @@
 Abstract classes for storage
 """
 
+from __future__ import annotations
 import abc
 import hashlib
 import itertools
@@ -42,14 +43,13 @@ from typing import Optional, Dict, Type, Any, List, Iterable, Tuple, Union, TYPE
 import click
 import yaml
 
-if TYPE_CHECKING:
-    # pylint: disable=cyclic-import
-    from ..client import Client
 from ..manifest.schema import Schema
 from ..manifest.manifest import Manifest
 from ..hashdb import HashDb, HashCache
 from ..link import Link
 from ..container import ContainerStub, Container
+if TYPE_CHECKING:
+    from ..client import Client # pylint: disable=cyclic-import
 
 BLOCK_SIZE = 1024 ** 2
 logger = logging.getLogger('storage')
@@ -628,7 +628,7 @@ class StorageBackend(metaclass=abc.ABCMeta):
         """
         raise OptionalError()
 
-    def add_child(self, client: 'Client', container: Container):
+    def add_child(self, client: Client, container: Container):
         """
         Add subcontainer to this storage.
 
@@ -636,7 +636,7 @@ class StorageBackend(metaclass=abc.ABCMeta):
         """
         raise OptionalError()
 
-    def remove_child(self, client: 'Client', container: Container):
+    def remove_child(self, client: Client, container: Container):
         """
         Remove subcontainer from this storage.
         """
