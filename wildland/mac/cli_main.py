dcbecb8c2cc4973c81a66d43d7024a104333a201
Piotr K. Isajew <piotr@wildland.io>
2021-07-30 09:37:07 +0200

    pylint

--- wildland/mac/cli_main.py
+++ wildland/mac/cli_main.py
@@ -1,14 +1,23 @@
+"""
+main program for the CLI on mac.
+"""
 import logging
+import sys
 
 from ..cli.cli_main import main
-from ..config import Config
 from ..envprovider import EnvProvider
 
 logger = logging.getLogger("cli_main")
 
 def cli_main(mountpoint: str):
+    """
+    Main method (supposed to be executed after bootstraping
+    Python in the CLI program. This essentially sets up
+    the configuration object and delegates further execution
+    to the Linux CLI.
+    """
     # initialize config with what we know
     cfg = EnvProvider.shared().load_config()
     cfg.override(override_fields = {'mount-dir': mountpoint})
     logger.debug("configuration bootstrapped with mountpoint %s", mountpoint)
-    exit(main(None, None, None, None, cfg))
+    sys.exit(main(None, None, None, None, cfg))
--- wildland/mac/env.py
+++ wildland/mac/env.py
@@ -1,10 +1,14 @@
+"""
+Darwin OS environment for Wildland.
+"""
+
 from ..wlenv import WLEnv
 from .config import MacConfig
 
 
 class MacEnv(WLEnv):
     """
-    macOS/Darwin environment for Wildland.
+    A WLEnv specialization for macOS/Darwin environment.
     """
 
     def load_config(self, params: dict = None) -> MacConfig:
