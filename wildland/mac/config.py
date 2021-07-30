160eab8cd332ca4e43a050592672692bfb40868e
Piotr K. Isajew <piotr@wildland.io>
2021-07-30 10:49:21 +0200

    mypy

--- wildland/mac/config.py
+++ wildland/mac/config.py
@@ -1,6 +1,27 @@
+# Wildland Project
+#
+# Copyright (C) 2020 Golem Foundation
+#
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 """
 Mac/Darwin implementation of Config
 """
+from pathlib import Path
 from ..config import Config
 
 
@@ -22,7 +43,7 @@ class MacConfig(Config):
         return MacConfig.__instance
 
     def __init__(self):
-        super().__init__(None, None, dict(), dict())
+        super().__init__(None, Path("."), dict(), dict())
         basecfg = Config.load()
         self.base_dir = basecfg.base_dir
         self.path = basecfg.path
