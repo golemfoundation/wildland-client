7fe62d291f70026313f9583ad8fc6d44fdb3c1ff
Piotr Bartman <prbartman@invisiblethingslab.com>
2021-09-24 19:26:36 +0200

    using ftruncate to implement truncate
    keep open file to use ftruncate() in truncate(): so we truncate only open files (that need to be saved). Thanks to this we never open an empty pseudomanifest.
    refactor of PseudomanifestFile: extract methods: _prepare_modify_cmd_args(), _update_error_message(); simplify write_full() and __init__()

--- wildland/storage_backends/pseudomanifest.py
+++ wildland/storage_backends/pseudomanifest.py
@@ -21,7 +21,7 @@
 import stat
 from itertools import chain
 from pathlib import PurePosixPath
-from typing import Dict, Any, Optional, List
+from typing import Dict, Any, Optional, List, Callable
 
 from .base import StorageBackend, File, Attr
 from .buffered import FullBufferedFile
@@ -40,87 +40,84 @@ class PseudomanifestFile(FullBufferedFile):
 
     def __init__(
             self, base_dir: Optional[str],
+            uuid_path: str,
+            container_wl_path: str,
             data: bytearray,
-            original_data: bytearray,
+            pseudomanifest_content: bytearray,
             error_message: bytearray,
-            attr: Attr
+            attr: Attr,
+            release_callback: Callable[["PseudomanifestFile"], None]
     ):
         super().__init__(attr)
         self.base_dir = base_dir
+        self.uuid_path = uuid_path
+        self.container_wl_path = container_wl_path
         self.data = data
-        self.original_data = original_data
-        self.message = error_message
-        manifest = Manifest.from_unsigned_bytes(bytes(self.original_data))
-        manifest.skip_verification()
-        self.uuid_path = manifest.fields['paths'][0]
-        self.container_wl_path = f"wildland:{manifest.fields['owner']}:{self.uuid_path}:"
+        self.pseudomanifest_content = pseudomanifest_content
+        self.error_message = error_message
+        self.release_callback = release_callback
+
+    def release(self, flags: int):
+        super().release(flags)
+        self.release_callback(self)
 
     def read_full(self) -> bytes:
         return self.data
 
     def write_full(self, data: bytes) -> int:
-        error_messages = ""
         try:
             new = Manifest.from_unsigned_bytes(bytes(data))
             new.skip_verification()
-            old = Manifest.from_unsigned_bytes(bytes(self.original_data))
+            old = Manifest.from_unsigned_bytes(bytes(self.pseudomanifest_content))
             old.skip_verification()
         except Exception as e:
-            error_messages += '\n' + str(e)
-        else:
-            args = []
+            self._update_error_message(data, str(e))
+            raise IOError()  # pylint: disable=raise-missing-from
+
+        args = self._prepare_modify_cmd_args(data, new, old)
+        if args:
             try:
-                args += self._add('path', new, old)
-                args += self._del('path', new, old)
-
-                args += self._add('category', new, old)
-                args += self._del('category', new, old)
+                _cli(self.base_dir, 'container', 'modify', self.container_wl_path, *args)
             except Exception as e:
-                error_messages += '\n' + str(e)
-
-            new_title = new.fields.get('title', None)
-            old_title = old.fields.get('title', None)
-            if new_title != old_title:
-                if new_title is None:
-                    new_title = "null"
-                args += ("--title", new_title)
-
-            new_other_fields = {key: value for key, value in new.fields.items()
-                                if key not in ('paths', 'categories', 'title')}
-            old_other_fields = {key: value for key, value in old.fields.items()
-                                if key not in ('paths', 'categories', 'title')}
-            if new_other_fields != old_other_fields:
-                error_messages += "\n Pseudomanifest error: Modifying fields except:" \
-                                  "\n 'paths', 'categories', 'title' are not supported."
-
-            if not error_messages and args:
-                try:
-                    _cli(self.base_dir, 'container', 'modify', self.container_wl_path, *args)
-
-                except Exception as e:
-                    error_messages += '\n' + str(e)
-
-        if error_messages:
-            str_data = data.decode()
-            lines = str_data.splitlines()
-            without_comments = [line for line in lines if not line.startswith("#")]
-            data_without_comments = "\n".join(without_comments)
-            message = \
-                '\n\n# Changes to the following manifest' \
-                '\n# was rejected due to encountered errors:' \
-                '\n#\n# ' + data_without_comments.replace('\n', '\n# ') + \
-                '\n# ' + error_messages.replace('\n', '\n# ') + \
-                '\n'
-            self.message[:] = message.encode()
-            self.data[:] = self.original_data + self.message
-            self.attr.size = len(self.data)
-            raise IOError()
-
-        # nothing has changed, keep a error message
-        self.data[:] = self.original_data + self.message
-        self.attr.size = len(self.data)
+                self._update_error_message(data, str(e))
+                raise IOError()  # pylint: disable=raise-missing-from
 
         return len(data)
+
+    def _prepare_modify_cmd_args(self, data: bytes, new: Manifest, old: Manifest) -> List[str]:
+        """Compare new and old manifests and return args to modify old manifest"""
+        args = []
+        error_messages = ""
+
+        try:
+            args += self._add('path', new, old)
+            args += self._del('path', new, old)
+
+            args += self._add('category', new, old)
+            args += self._del('category', new, old)
+        except Exception as e:
+            error_messages += '\n' + str(e)
+
+        new_title = new.fields.get('title', None)
+        old_title = old.fields.get('title', None)
+        if new_title != old_title:
+            if new_title is None:
+                new_title = "null"
+            args += ("--title", new_title)
+
+        new_other_fields = {key: value for key, value in new.fields.items()
+                            if key not in ('paths', 'categories', 'title')}
+        old_other_fields = {key: value for key, value in old.fields.items()
+                            if key not in ('paths', 'categories', 'title')}
+        if new_other_fields != old_other_fields:
+            error_messages += "\n Pseudomanifest error: Modifying fields except:" \
+                              "\n 'paths', 'categories', 'title' are not supported."
+
+        if error_messages:
+            self._update_error_message(data, error_messages)
+            raise IOError()
+
+        return args
 
     def _add(self, field: str, new: Manifest, old: Manifest) -> List[str]:
         return self._modify('add', field, new, old)
@@ -153,6 +150,24 @@ class PseudomanifestFile(FullBufferedFile):
 
         return list(chain.from_iterable((f'--{mod}-{field}', f) for f in to_modify))
 
+    def _update_error_message(self, data: bytes, error_messages: str):
+        """Append error messages to the unchanged pseudomanifest"""
+        str_data = data.decode()
+        lines = str_data.splitlines()
+        without_comments = [line for line in lines if not line.startswith("#")]
+        data_without_comments = "\n".join(without_comments)
+
+        message = \
+            '\n\n# Changes to the following manifest' \
+            '\n# was rejected due to encountered errors:' \
+            '\n#\n# ' + data_without_comments.replace('\n', '\n# ') + \
+            '\n# ' + error_messages.replace('\n', '\n# ') + \
+            '\n'
+
+        self.error_message[:] = message.encode()
+        self.data[:] = self.pseudomanifest_content + self.error_message
+        self.attr.size = len(self.data)
+
 
 class PseudomanifestStorageBackend(StorageBackend):
     """
@@ -172,19 +187,26 @@ class PseudomanifestStorageBackend(StorageBackend):
 
     def __init__(self, *, params: Dict[str, Any], **kwds):
         super().__init__(params=params, **kwds)
+
         self.read_only = False
+
+        self.base_dir = params.get('base-dir', None)
+        if self.base_dir == 'None':
+            self.base_dir = None
 
         data = params['content']
         if isinstance(data, str):
             data = data.encode()
         header = b'# All YAML comments will be discarded when the manifest is saved\n'
         self.data = bytearray(header + data)
-        self.original_data = self.data.copy()
-        self.error_message = bytearray(b"")
 
-        self.base_dir = params.get('base-dir', None)
-        if self.base_dir == 'None':
-            self.base_dir = None
+        manifest = Manifest.from_unsigned_bytes(bytes(self.data))
+        manifest.skip_verification()
+
+        self.uuid_path = manifest.fields['paths'][0]
+        self.container_wl_path = f"wildland:{manifest.fields['owner']}:{self.uuid_path}:"
+        self.pseudomanifest_content = self.data.copy()
+        self.error_message = bytearray(b"")
 
         self.attr = Attr(
             size=len(self.data),
@@ -192,42 +214,50 @@ class PseudomanifestStorageBackend(StorageBackend):
             mode=stat.S_IFREG | 0o666
         )
 
+        self.open_files: List[File] = []
+
     def open(self, path: PurePosixPath, flags: int) -> File:
         """
         open() for generated pseudomanifest storage
         """
-        self.attr.size = len(self.data)
         file = PseudomanifestFile(
             self.base_dir,
+            self.uuid_path,
+            self.container_wl_path,
             self.data,
-            self.original_data,
+            self.pseudomanifest_content,
             self.error_message,
-            self.attr
+            self.attr,
+            release_callback=self.open_files.remove
         )
+        self.open_files.append(file)
         return file
 
     def getattr(self, path: PurePosixPath) -> Attr:
         """
         getattr() for generated pseudomanifest storage
         """
-        self.attr.size = len(self.data)
         return self.attr
 
     def truncate(self, path: PurePosixPath, length: int) -> None:
         """
         Truncate the pseudomanifest file.
         """
-        if length != 0:
+        if not self.open_files:
             raise NotImplementedError()
-        self.data.clear()
+
+        for file in self.open_files:
+            file.ftruncate(length)
 
 
 def _cli(base_dir, *args):
     # pylint: disable=import-outside-toplevel,cyclic-import
     from ..cli import cli_main
     cmdline = ['--base-dir', base_dir, *args] if base_dir else args
+
     # Convert Path to str
     cmdline = [str(arg) for arg in cmdline]
+
     try:
         cli_main.main.main(args=cmdline, prog_name='wl')
     except SystemExit as e:
