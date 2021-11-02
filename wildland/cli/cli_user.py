0c3cfc575649e27ebf3461a9f72724932994d56b
Aleksander Kapera <aleksander.kapera@besidethepark.com>
2021-11-02 11:13:23 +0100

    Code review changes

--- wildland/cli/cli_user.py
+++ wildland/cli/cli_user.py
@@ -282,11 +282,8 @@ def _remove_suffix(s: str, suffix: str) -> str:
     return s
 
 
-def _do_import_manifest(obj, path_or_dict,
-                        manifest_owner: Optional[str] = None,
-                        force: bool = False,
-                        paths: Optional[List[PurePosixPath]] = None,
-                        default_user: Optional[str] = None) -> Tuple[Optional[Path], Optional[str]]:
+def _do_import_manifest(obj, path_or_dict, manifest_owner: Optional[str] = None,
+                        force: bool = False) -> Tuple[Optional[Path], Optional[str]]:
     """
     Takes a user or bridge manifest as pointed towards by path (can be local file path, url,
     wildland url), imports its public keys, copies the manifest itself.
@@ -352,13 +349,13 @@ def _do_import_manifest(obj, path_or_dict,
             if user.owner == imported_user.owner:
                 if not force:
                     if any(user.owner == b.user_id for b in obj.client.get_local_bridges()):
-                        click.echo(f"User {user.owner} and theirs bridge already exist. "
+                        click.echo(f"User {user.owner} and their bridge already exist. "
                                    f"Skipping import.")
-                    elif default_user:
-                        file_path = obj.client.local_url(Path(user.manifest.local_path).absolute())
-                        _do_process_imported_manifest(obj, user.manifest.local_path,
-                                                      file_path, paths, default_user)
-                    return None, None
+                        return None, None
+
+                    click.echo(f"User {user.owner} already exists. Creating their bridge.")
+                    file_path = obj.client.local_url(Path(user.manifest.local_path).absolute())
+                    return user.manifest.local_path, file_path
 
                 click.echo(f'User {user.owner} already exists. Forcing user import.')
                 user_exists = True
@@ -417,7 +414,7 @@ def find_user_manifest_within_catalog(obj, user: User) -> \
 
 def _do_process_imported_manifest(
         obj: ContextObj, copied_manifest_path: Path, user_manifest_location: str,
-        paths: Optional[List[PurePosixPath]], default_user: str):
+        paths: List[PurePosixPath], default_user: str):
     """
     Perform followup actions after importing a manifest: create a Bridge manifest for a user,
     import a Bridge manifest's target user
@@ -495,9 +492,7 @@ def import_manifest(obj: ContextObj, path_or_url: str, paths: Iterable[str],
     posix_paths = [PurePosixPath(p) for p in paths]
 
     if wl_obj_type == WildlandObject.Type.USER:
-        copied_manifest_path, manifest_url = _do_import_manifest(obj, path_or_url,
-                                                                 paths=posix_paths,
-                                                                 default_user=default_user)
+        copied_manifest_path, manifest_url = _do_import_manifest(obj, path_or_url)
         if not copied_manifest_path or not manifest_url:
             return
         try:
