975e2ea27095ab50bedd112ac1d0aaa156845524
Dominik Gonciarz <dominik.gonciarz@besidethepark.com>
2022-01-11 14:52:39 +0100

    filter repeated containers to un/publish

--- wildland/cli/cli_common.py
+++ wildland/cli/cli_common.py
@@ -535,8 +535,17 @@ def _get_publishable_object_from_file_or_path(
     if wl_object_type == WildlandObject.Type.CONTAINER and WildlandPath.match(path):
         wlpath = WildlandPath.from_str(path)
         if wlpath.file_path is None:
-            return obj.client.load_containers_from(wlpath, {
+            containers = obj.client.load_containers_from(wlpath, {
                 'default': obj.client.config.get('@default')})
+            containers_dict: Dict[Container] = {}
+            for container in containers:
+                # same container can be present multiple times. Take only the first one, to match
+                # the current behavior of load_object_from_url.
+                container_id = f'{container.uuid}:{container.owner}'
+                if containers_dict.get(container_id) is not None:
+                    continue
+                containers_dict[container_id] = container
+            return [containers_dict[c_id] for c_id in containers_dict]
 
     wl_object = obj.client.load_object_from_name(wl_object_type, path)
 
